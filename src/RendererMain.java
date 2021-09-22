0ee4b4cb2eda83d36343d65ac180cad17bb58b0a
nil0202 <57811775+nil0202@users.noreply.github.com>
2021-09-22 06:31:57 -0400

    Fixed formatting

--- src/RendererMain.java
+++ src/RendererMain.java
@@ -44,16 +44,16 @@ public class RendererMain extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Scene scene =
-        		new Scene(
-        		new ImagePlane(new Vector3d(1, 0.75, 0), new Vector3d(-1, 0.75, 0), new Vector3d(1, -0.75, 0), new Vector3d(-1, -0.75, 0)),
-        		new Camera(new Vector3d(0, 0, -1)),
-        		new ArrayList<Sphere>(Arrays.asList(new Sphere(new Vector3d(0, 0, 1.2), 0.8, new Material(new MyColor(0.2, 0.1, 0.1), new MyColor(0.4, 0.1, 0.1), new MyColor(0.7, 0.7, 0.7), 100, new MyColor(0.1, 0.1, 0.1))),
-        		new Sphere(new Vector3d(1.7, 0.3, 2), 0.5, new Material(new MyColor(0, 0.73, 0.73), new MyColor(0.4, 0.1, 0.1), new MyColor(0.7, 0.7, 0.7), 50, new MyColor(0.2, 0.1, 0.16))),
-        		new Sphere(new Vector3d(-1.6, 0.6, 2), 0.8, new Material(new MyColor(1, 0.61, 0), new MyColor(0.6, 0.4, 0), new MyColor(0.4, 0.6, 0.6), 80, new MyColor(0.1, 0.2, 0.1))))),
-        		new ArrayList<Light>(Arrays.asList(new Light(new Vector3d(5, 2.5, 0.5), new MyColor(0.8, 0.8, 0.8), new MyColor(0.8, 0.8, 0.8)),
-        		new Light(new Vector3d(-5, 2.5, 0.5), new MyColor(0.7, 0.7, 0.7), new MyColor(0.8, 0.8, 0.8)))),
-        		new MyColor(0.8, 0.8, 0.8)
-        		);
+        	new Scene(
+        	new ImagePlane(new Vector3d(1, 0.75, 0), new Vector3d(-1, 0.75, 0), new Vector3d(1, -0.75, 0), new Vector3d(-1, -0.75, 0)),
+        	new Camera(new Vector3d(0, 0, -1)),
+        	new ArrayList<Sphere>(Arrays.asList(new Sphere(new Vector3d(0, 0, 1.2), 0.8, new Material(new MyColor(0.2, 0.1, 0.1), new MyColor(0.4, 0.1, 0.1), new MyColor(0.7, 0.7, 0.7), 100, new MyColor(0.1, 0.1, 0.1))),
+        	new Sphere(new Vector3d(1.7, 0.3, 2), 0.5, new Material(new MyColor(0, 0.73, 0.73), new MyColor(0.4, 0.1, 0.1), new MyColor(0.7, 0.7, 0.7), 50, new MyColor(0.2, 0.1, 0.16))),
+        	new Sphere(new Vector3d(-1.6, 0.6, 2), 0.8, new Material(new MyColor(1, 0.61, 0), new MyColor(0.6, 0.4, 0), new MyColor(0.4, 0.6, 0.6), 80, new MyColor(0.1, 0.2, 0.1))))),
+        	new ArrayList<Light>(Arrays.asList(new Light(new Vector3d(5, 2.5, 0.5), new MyColor(0.8, 0.8, 0.8), new MyColor(0.8, 0.8, 0.8)),
+        	new Light(new Vector3d(-5, 2.5, 0.5), new MyColor(0.7, 0.7, 0.7), new MyColor(0.8, 0.8, 0.8)))),
+        	new MyColor(0.8, 0.8, 0.8)
+        	);
 
         for (int x = 0; x < width; x++) {
         	for(int y = 0; y < height; y++){
