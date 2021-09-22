25ccf106c72c05f224da783b2a229dcd09683027
nil0202 <57811775+nil0202@users.noreply.github.com>
2021-09-22 06:25:40 -0400

    Fixed formatting

--- src/Ray.java
+++ src/Ray.java
@@ -4,10 +4,11 @@ public class Ray {
 	private final Vector3d direction;
 	
 	public Ray(double x, double y) {
-		double alpha = x/(double) RendererMain.width;
-		double beta = y/(double) RendererMain.height;
-		Vector3d t = Vector3d.add((Vector3d.multiply((1 - alpha), ImagePlane.getX1())), (Vector3d.multiply((alpha), ImagePlane.getX2())));
-		Vector3d b = Vector3d.add((Vector3d.multiply((1 - alpha), ImagePlane.getX3())), (Vector3d.multiply((alpha), ImagePlane.getX4())));
+	    double alpha = x/(double) RendererMain.width;
+	    double beta = y/(double) RendererMain.height;
+		
+	    Vector3d t = Vector3d.add((Vector3d.multiply((1 - alpha), ImagePlane.getX1())), (Vector3d.multiply((alpha), ImagePlane.getX2())));
+	    Vector3d b = Vector3d.add((Vector3d.multiply((1 - alpha), ImagePlane.getX3())), (Vector3d.multiply((alpha), ImagePlane.getX4())));
 
 	    this.origin = Vector3d.add((Vector3d.multiply((1 - beta), t)), (Vector3d.multiply((beta), b)));
 	    this.direction = Vector3d.subtract(this.origin, Camera.getCamera());
